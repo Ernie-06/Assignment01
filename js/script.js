7c8431eb80325a4a19138b5c656a23ace6aee07d
Ernaldo Becerra <ernaldito12@gmail.com>
2024-09-24 22:22:49 -0700

    converted camel case and added expressions

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Here are the identifiers converted to Camel Case notation:
+
+let someMonth 
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// numeric literal expression 
+let age = 25;
+// string literal expression
+let greeting = "Hello, world!";
+// boolean literal expression
+let isActive = true;
+// null literal expression
+let value = null;
