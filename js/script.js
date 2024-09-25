5d7edb535f6e1985ac2cf7e7615fc162ad086ba1
Ernaldo Becerra <ernaldito12@gmail.com>
2024-09-24 22:49:43 -0700

    added complex,declared variables, variables

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Here are the identifiers converted to Camel Case notation:
 
 let someMonth 
-function theMonth()
+function theMonth() {}
 let currentMonth
 let summerMonth
 let myLibraryFunction
@@ -14,3 +14,22 @@ let greeting = "Hello, world!";
 let isActive = true;
 // null literal expression
 let value = null;
+// Complex or Variable Expressions
+let anExpression = 3 + 6
+let a = 10;
+// Here are the declared variables using Camel Case and Hungarian Notation:
+
+let strFirstName;      // String for First Name
+let strLastName;       // String for Last Name
+let strAddress;        // String for Address
+let strCity;           // String for City
+let strState;          // String for State
+let strZipCode;        // String for Zip Code
+let intYourAge;        // Integer for Your Age
+let strReferralSource; // String for Referral Source
+let boolMayWeContactYou; // Boolean for May We Contact You
+//Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
+let strFirstName = "Ernaldo";
+let strLastName = "Becerra";
+let intYourAge = 26;
+//
